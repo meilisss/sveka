e736ffb9432227c796f37cde73392cfa56af391a
Meylis <meylis.ovezov.98@gmail.com>
2022-10-04 12:07:00 +0500

    feat: add tailwind css and postcss preprocessor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit + Ory Kratos Implementation</h1>
+<p>Everything has been started from here!</p>
+
