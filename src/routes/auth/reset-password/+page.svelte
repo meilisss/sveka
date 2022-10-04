499afa0f6d104c54dd72844eb5f33ac28d4f5567
Meylis <meylis.ovezov.98@gmail.com>
2022-10-04 14:02:04 +0500

    ui: add auth pages

---
+++ src/routes/auth/reset-password/+page.svelte
@@ -0,0 +1,28 @@
+<div class="auth">
+  <!-- component -->
+  <div class="flex justify-center self-center  z-10">
+    <div class="p-12 bg-white mx-auto rounded-3xl w-96 ">
+      <div class="mb-7">
+        <h3 class="font-semibold text-2xl text-gray-800">Password reset</h3>
+      </div>
+      <div class="space-y-6">
+        <div class="">
+          <input class=" w-full text-sm  px-4 py-3 bg-gray-200 focus:bg-gray-100 border  border-gray-200 rounded-lg focus:outline-none focus:border-purple-400" type="" placeholder="Email">
+        </div>
+          <div>
+            <button type="submit" class="w-full flex justify-center bg-purple-800  hover:bg-purple-700 text-gray-100 p-3  rounded-lg tracking-wide font-semibold  cursor-pointer transition ease-in duration-500">
+              Confirm email
+            </button>
+          </div>
+        </div>
+        <div class="mt-7 text-center text-gray-300 text-xs">
+          <span>
+            Copyright © 2022
+            <a href="https://ids.company" rel="" target="_blank" title="IDS Company" class="text-purple-500 hover:text-purple-600 ">ids.company</a>
+          </span>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js"></script>
+  </div>
+
